da70213047f0389b613cbd96fdb12678addbbeeb
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-07 22:35:50 +0900

    first commit

---
+++ push-notification/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  push-notification
+//
+//  Created by Nakagawa Kosuke on 2023/08/02.
+//
+
+import UIKit
+import FirebaseCore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+      
+    }
+
+
+}
+
